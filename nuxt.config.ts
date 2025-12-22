655fb7c485e610b1d71b8d7b01d6c284c368306b
Shirsendu Bairagi <shirsendu2001@gmail.com>
2025-12-22 22:01:48 +0530

    chore: update nuxt.config.ts for media image baseURL and adjust native config formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -157,7 +157,7 @@ export default defineNuxtConfig({
   image: {
     provider: 'ipx',
     ipx: {
-      baseURL: `${process.env.NUXT_PUBLIC_CDN_URL}/image`,
+      baseURL: `${process.env.NUXT_PUBLIC_CDN_URL}/media/image`,
       modifiers: {
         format: 'auto',
         quality: 80,
