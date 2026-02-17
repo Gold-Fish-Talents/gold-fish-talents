5a46858e8a781a2f63b3a88efd2f95b0be6531d6
SATADRU1 <satadrumondal1331@gmail.com>
2026-02-17 21:52:37 +0530

    fix: type error updated

--- server/utils/typesense.ts
+++ server/utils/typesense.ts
@@ -1,5 +1,7 @@
 import Typesense from 'typesense'
 
+// Force update for lint check
+
 const typesenseClientSingleton = () => {
   return new Typesense.Client({
     nodes: [{ host: process.env.TYPESENSE_HOST!, port: process.env.TYPESENSE_PORT!, protocol: 'http' }],
@@ -7,12 +9,12 @@ const typesenseClientSingleton = () => {
   })
 }
 
-declare const globalThis: {
-  typesenseGlobal: ReturnType<typeof typesenseClientSingleton>
-} & typeof global
+declare global {
+  var typesenseGlobal: ReturnType<typeof typesenseClientSingleton> | undefined
+}
 
 const typesense = globalThis.typesenseGlobal ?? typesenseClientSingleton()
 
 export default typesense
 
-if (process.env.NODE_ENV !== 'production') globalThis.typesenseGlobal = typesense
+if (import.meta.env.NODE_ENV !== 'production') globalThis.typesenseGlobal = typesense
